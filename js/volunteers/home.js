87a8161f01d355a0eaf16989fc13025f48738019
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-06-02 23:15:04 -0700

    Updated volunteer home page

--- js/volunteers/home.js
+++ js/volunteers/home.js
@@ -1,200 +1,106 @@
-import {
-    checkUserAuthorization
-} from "../firebase/authentication.js";
-import {
-    signOut
-} from "../utils.js";
-import{
-    getAll,
-    deleteDocument,
-    updateDocument,
-    createDocument
-} from "../firebase/firestore.js";
+import { checkUserAuthorization } from "../firebase/authentication.js";
+import { signOut } from "../utils.js";
+import { getAll, deleteDocument, updateDocument, createDocument } from "../firebase/firestore.js";
+import { createMapView } from "../map.js";
 
 checkUserAuthorization();
 
 /**
- * This adds an event listener to the page that triggers once everything is done downloading. 
+ * This adds an event listener to the page that triggers once everything is done downloading.
  * This is to prevent the code from trying to access an element from the page before it was
  * rendered there
- * 
+ *
+ * Commented by Yosuke on 2 June 2024:
+ * Due to async, it seems that some times the DOMContentLoaded event is not triggered.
+ * So, check the readyState of the document if it's alreadt "complete" to run the code.
  */
-document.addEventListener("DOMContentLoaded",()=>{
-    const logoutBtn = document.getElementById("logoutBtn");
-    const table = document.getElementsByTagName("tbody")[0];
-    const taskForm = document.getElementById("taskForm");
-    const formOperation = document.getElementById("operationType")
-    const formHeader = document.querySelector("#taskForm h2");
-    const formButton = document.querySelector("#taskForm button[type=submit]");
-    const cancelEditingButton = document.getElementById("cancelBtn");
 
-    //default styling
-    cancelEditingButton.style.display = "none";
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", runFunction);
+} else {
+  console.log(document.readyState);
+  runFunction();
+}
 
-    cancelEditingButton.addEventListener("click", resetForm);
-    logoutBtn.addEventListener("click",signOut);
+function runFunction() {
+  // const table = document.getElementsByTagName("tbody")[0];
+  const listSection = document.getElementById("listSection");
+  populateTaskList();
 
-    //Stores the id of the task selected to be edited
-    let selectedTaskID = null;
+  /**
+   * Fetches data from the "Tasks" collection of Firestoer Database
+   * and displays on a table
+   */
+  async function populateTaskList() {
+    try {
+      let allTasks = await getAll("tasks");
+      console.log(allTasks);
 
-    populateTable();
+      // Create map view
+      createMapView(allTasks);
 
-    /**
-     * Resets the Task form to the default state (Add Task)
-     */
-    function resetForm(){
-        cancelEditingButton.style.display = "none";
-        formOperation.value = "add";
-        formHeader.innerText = "Add Task";
-        formButton.innerText = "Add";
-        taskForm.reset();
+      // Create card view
+      for (let task of allTasks) {
+        let id = task[0];
+        let taskDetails = task[1];
+
+        const card = document.createElement("div");
+        card.classList.add("taskCard");
+        card.innerHTML = `
+          <h2>${taskDetails.name}</h2>
+          <p>${taskDetails.status}</p>
+          <p>${taskDetails.requester}</p>
+          <button><a href="./task-detail.html">See more</button>
+        `;
+        listSection.appendChild(card);
+      }
+    } catch (error) {
+      console.log(error);
+      alert(error);
     }
-    /**
-     * Fetches data from the "Tasks" collection of Firestoer Database
-     * and displays on a table
-     */
-    async function populateTable(){
-        try{
-            let allTasks = await getAll("tasks");
+  }
 
-            table.innerHTML = "";
+  /**
+   * Tab menu in home page
+   */
+  var tabLinks = document.querySelectorAll(".tab4__link");
+  var tabBodyItems = document.querySelectorAll(".tab4-body__item");
 
-            for(let task of allTasks){
-                let id = task[0];
-                let taskDetails = task[1];
+  tabLinks.forEach(function (tabLink) {
+    tabLink.addEventListener("click", function (e) {
+      // Show and hide tabs
+      tabLinks.forEach(function (link) {
+        link.classList.remove("on");
+      });
+      e.currentTarget.classList.add("on");
 
-                let row = table.insertRow();
-                let cell = row.insertCell();
+      let num = e.currentTarget.getAttribute("data-tab-body");
+      tabBodyItems.forEach(function (item) {
+        item.classList.remove("on");
+      });
+      document.querySelector(".tab4-body__item--" + num).classList.add("on");
+    });
+  });
 
-                cell.innerText = id;
-                cell.id = `task_id_${id}`; //adding unique identifiers for each cell with the id of the task
-
-                cell = row.insertCell();
-                cell.id = `task_name_${id}`;
-                cell.innerText = taskDetails.name;
-
-                cell = row.insertCell();
-                cell.id = `task_status_${id}`;
-                cell.innerText = taskDetails.status;
-
-                cell = row.insertCell();
-                cell.id = `task_requesterid_${id}`;
-                cell.innerText = taskDetails.requester;
-
-                cell = row.insertCell();
-                cell.innerHTML = `
-                <button id="deleteTask_${id}" class="btn btn-outline-danger deleteButton" type"button">Delete</button>
-                <button id="editTask_${id}" class="btn btn-outline-secondary editButton" type"button">Edit</button>
-                `;
-            }
-
-            let deleteButtons = Array.from(document.getElementsByClassName("deleteButton"));
-
-            deleteButtons.forEach((button)=>{
-                button.addEventListener("click", async (e)=>{
-                    let taskID = button.id.split("_")[1];
-                    
-                    try{
-                        //TODO: call delete function with the task ID and then populate table again
-                        await deleteDocument("tasks",taskID);
-                        populateTable();
-                    }catch(error){
-                        console.log(error);
-                    }
-                })
-            })
-            let editButtons = Array.from(document.getElementsByClassName("editButton"));
-
-            editButtons.forEach((button)=>{
-                button.addEventListener("click", ()=>{
-                    //when the edit button is clicked, the form will switch to be a edition form instead of a creation form
-
-                    formOperation.value = "edit"; //this formOperation will determine which action will be performed when the form is submitted
-                    cancelEditingButton.style.display = "block"; //show cancel button
-                    
-                    let taskID = button.id.split("_")[1];
-                    selectedTaskID = taskID;
-                    
-                    let taskNameToBeUpdated = document.getElementById(`task_name_${taskID}`).innerText;
-                    let taskNameFormField = document.getElementById("taskName");
-                    taskNameFormField.value = taskNameToBeUpdated; 
-
-                    let taskRequesterToBeUpdated = document.getElementById(`task_requesterid_${taskID}`).innerText;
-                    let taskRequesterFormField = document.getElementById("requester");
-                    taskRequesterFormField.value = taskRequesterToBeUpdated;
-
-                    formHeader.innerText = "Edit Task";
-                    formButton.innerText = "Update";
-                })
-            })
-        }catch(error){
-            console.log(error);
-            alert(error);
-        }
-    }
-    taskForm.addEventListener("submit",async (e)=>{
-        e.preventDefault();
-        if(formOperation.value == "add"){
-            let newTask = {
-                name: document.getElementById("taskName").value, //from the form
-                status: "In progress", //by default
-                requester: document.getElementById("requester").value //from the form
-                //note: no need to pass an ID for the task, because firebase will generate an ID for it automatically if we don't provide one
-            }
-            try{
-                createTask(newTask);
-                populateTable();
-                taskForm.reset();
-            }catch(error){
-                console.log(error);
-            }
-        }else{
-            let updatedTask = {
-                name: document.getElementById("taskName").value, //from the form
-                status: document.getElementById(`task_status_${selectedTaskID}`).innerText, //from the table
-                requester: document.getElementById("requester").value //from the form
-            }
-            try{
-                updateTask(selectedTaskID,updatedTask);
-                populateTable();
-                taskForm.reset();
-            }catch(error){
-                console.log(error);
-            }
-        }
-    })
-        /**
-     * Deletes a task based on the id of the task
-     * @param {Object} the object that represents the task 
-     */
-    async function createTask(obj){
-        try{
-            createDocument("tasks", obj);
-        }catch(error){
-            console.log(error);
-        }
-    }
-    /**
-     * Deletes a task based on the id of the task
-     * @param {string} id 
-     */
-    async function deleteTask(id){
-        try{
-            deleteDocument("tasks",id);
-        }catch(error){
-            console.log(error);
-        }
-    }
-    /**
-     * Updates a task based on the id of the task
-     * @param {string} id - Task ID
-     * @param {Object} obj - object that represents the updated task
-     */
-    async function updateTask(id,obj){
-        try{
-            updateDocument("tasks", id, obj);
-        }catch(error){
-            console.log(error);
-        }
-    }
-})
+  // Switch between map and list view (toggle hide class)
+  const mapView = document.getElementById("mapView");
+  const listView = document.getElementById("listView");
+  mapView.addEventListener(
+    "change",
+    (event) => {
+      if (event.cancelable) event.preventDefault();
+      document.getElementById("mapSection").classList.remove("hide");
+      document.getElementById("listSection").classList.add("hide");
+    },
+    { passive: false }
+  );
+  listView.addEventListener(
+    "change",
+    (event) => {
+      if (event.cancelable) event.preventDefault();
+      document.getElementById("mapSection").classList.add("hide");
+      document.getElementById("listSection").classList.remove("hide");
+    },
+    { passive: false }
+  );
+}
