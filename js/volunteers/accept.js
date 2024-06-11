53f2fc445e081fa6ac9dfd938782f0a36e950d7f
terustestacount <terumasamori@gmail.com>
2024-06-10 23:54:33 -0700

    WIP Volunteer accept task JS part.

--- js/volunteers/accept.js
+++ js/volunteers/accept.js
@@ -5,3 +5,74 @@ function on() {
 function off() {
     document.getElementById("overlay").style.display = "none";
 }
+
+
+import { getCurrentUserID } from "../firebase/authentication.js";
+import { updateDocument, getDocument } from "../firebase/firestore.js";
+
+let taskID;
+let taskData = {};
+
+/**
+ * This adds an event listener to the page that triggers once everything is done downloading.
+ * This is to prevent the code from trying to access an element from the page before it was
+ * rendered there
+ *
+ */
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", runFunction);
+} else {
+  runFunction();
+}
+
+function runFunction() {
+  // Get task ID from URL
+  const urlParams = new URLSearchParams(window.location.search);
+  taskID = urlParams.get("taskid");
+  console.log(taskID);
+
+  // Get the task data from the Firestore
+  getDocument("tasks", taskID)
+    .then((task) => {
+      console.log(task);
+      // Save the task data to a global variable
+      taskData = task;
+      getDocument("users", task.requesterID)
+        .then((user) => {
+            console.log(user.lastName);
+            console.log(user.address);        
+        });
+      console.log(taskData.details.address);
+      // ToDo: Display task data on the page
+      // Code here...
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+document.getElementById("completeBtn").addEventListener("click", function () {
+//   acceptTask(taskID, taskData);
+// on();
+});
+
+async function acceptTask(taskID, taskData) {
+  console.log(taskData);
+
+  // Get the volunteer ID
+  const volunteerID = getCurrentUserID();
+
+  // Create updated task data object with the volunteer ID and status "On going"
+  taskData.volunteerID = volunteerID;
+  taskData.status = "On going";
+  console.log(taskData);
+
+  // Update the task data on the Firestore
+  updateDocument("tasks", taskID, taskData)
+    .then(() => {
+      console.log("Task accepted!");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
