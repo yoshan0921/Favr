14c0041b9268a6a879c46d93c613fc11ed98b653
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-06-20 16:35:23 -0700

    Added chat function

--- js/firebase/firebase.js
+++ js/firebase/firebase.js
@@ -2,19 +2,21 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA98KYP3hvGX8q7uk1WNdEStxMo1S85HmA",
-    authDomain: "integrated-project-pwa.firebaseapp.com",
-    projectId: "integrated-project-pwa",
-    storageBucket: "integrated-project-pwa.appspot.com",
-    messagingSenderId: "773054560330",
-    appId: "1:773054560330:web:49b001fd6ab030f08c4eb3",
-    measurementId: "G-BWXW6C61EP"
-  };
+  apiKey: "AIzaSyA98KYP3hvGX8q7uk1WNdEStxMo1S85HmA",
+  authDomain: "integrated-project-pwa.firebaseapp.com",
+  projectId: "integrated-project-pwa",
+  storageBucket: "integrated-project-pwa.appspot.com",
+  messagingSenderId: "773054560330",
+  appId: "1:773054560330:web:49b001fd6ab030f08c4eb3",
+  measurementId: "G-BWXW6C61EP",
+};
 
 // Initialize Firebase
 export const firebase = initializeApp(firebaseConfig);
 export const auth = getAuth(firebase);
 export const firestore = getFirestore(firebase);
 export const storage = getStorage(firebase);
+export const database = getDatabase(firebase);
