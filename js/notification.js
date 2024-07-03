bb4c73f1d4d481dea252be3b460f824c6093b9ef
v1n1c1u <vinicius.abner1039@gmail.com>
2024-07-03 08:50:34 -0700

    adding basic notification triggering and receiving

--- js/notification.js
+++ js/notification.js
@@ -1,42 +1,71 @@
 import { loadPartial } from "./common.js";
 import { getCurrentUserID } from "./firebase/authentication.js";
 import { database } from "./firebase/firebase.js";
-import { ref, child, query, push, get, onChildAdded, orderByKey, limitToLast } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { ref, child, query, push, get, onChildAdded, onValue, orderByKey, update } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const currentUserID = getCurrentUserID();
 
 function listenToNotifications(){
     onChildAdded(ref(database, currentUserID), async function (data) {
         const v = data.val();
-        await loadPartial("_notification", "body");
         console.log(v);
-        const notificationCard = document.getElementById("notificationCard");
-        const notificationLink = document.querySelector(".notification a");
-        const notificationIcon = document.querySelector(".notification #notificationIcon");
-        const notificationTitle = document.querySelector(".notification a h2");
-        const notificationText = document.querySelector(".notification a p");
-        notificationLink.setAttribute("href", v.link)
-        if(v.icon && v.icon !== "#") notificationIcon.setAttribute("src",v.icon);
-        notificationTitle.innerText = v.title;
-        notificationText.innerText = v.message;
-        notificationCard.classList.add("show");
-        const notificationCloseButton = document.getElementsByClassName("closeNotificationModal")[0];
-        notificationCloseButton.addEventListener("click",()=>{
-            notificationCard.classList.remove("show");
-            document.getElementsByTagName("body")[0].removeChild(notificationCard);
-        })
+        if(v.isNew){
+            const updatesMenu = document.querySelector("#updates-menu");
+            updatesMenu.classList.add("has-updates");
+            if(!v.wasSent){
+                await loadPartial("_notification", "body");
+                const notificationCard = document.getElementById("notificationCard");
+                const notificationLink = document.querySelector(".notification a");
+                const notificationIcon = document.querySelector(".notification #notificationIcon");
+                const notificationTitle = document.querySelector(".notification a h2");
+                const notificationText = document.querySelector(".notification a p");
+                notificationLink.href = v.link;
+                if(v.icon && v.icon !== "#") notificationIcon.setAttribute("src",v.icon);
+                notificationTitle.innerText = v.title;
+                notificationText.innerText = v.message;
+                notificationCard.classList.add("show");
+                const notificationCloseButton = document.getElementsByClassName("closeNotificationModal")[0];
+                notificationCloseButton.addEventListener("click",()=>{
+                    notificationCard.classList.remove("show");
+                    document.body.removeChild(notificationCard);
+                })
+                const updateObj = {};
+                v.wasSent = true;
+                updateObj[`/${currentUserID}/${v.notificationID}`] = v;
+                update(ref(database),updateObj);
+            }
+        }
     });
 }
-function sendNotification(data,receiverID){
-    push(ref(database, receiverID), {
+async function sendNotification(data,receiverID){
+    const notificationObj = {
+        id: "",
         title: data.title,
-        message: data.message
-    }).catch((error) => {
-        console.error("Failed to save data:", error);
-    });
+        message: data.message,
+        icon: data.icon,
+        link: data.link,
+        time: new Date(),
+        isNew: true,
+        wasSent: false
+    }
+    const notificationID = push(child(ref(database), receiverID)).key;
+    notificationObj["id"] = notificationID;
+    const updateObj = {};
+    updateObj[`/${receiverID}/${notificationID}`] = notificationObj;
+    update(ref(database),updateObj);
 }
-function loadAllNotifications(){
-
+function loadAllNotifications(userID){
+    onValue(ref(database, userID), (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+          const childKey = childSnapshot.key;
+          const childData = childSnapshot.val();
+          console.log(childKey, childData);
+          // ...
+        });
+        return snapshot;
+      }, {
+        onlyOnce: true
+      });
 }
 export {
     listenToNotifications,
---
+++ js/updates.js
@@ -0,0 +1,35 @@
+import { getCurrentUserID } from "./firebase/authentication"
+import { loadAllNotifications } from "./notification.js";
+
+document.addEventListener("DOMContentLoaded",async ()=>{
+    var notifications = loadAllNotifications(getCurrentUserID());
+    const updatesList = document.getElementById("updates");
+    try{
+        updatesList.innerHTML = "";
+        notifications.forEach((notification)=>{
+            const card = document.createElement("a");
+            card.href = notification.link;
+            const notificationIcon = document.createElement("div");
+            notificationIcon.classList.add("icon-wrapper");
+            notificationIcon.innerHTML = `
+                <img src="${notification.icon}" alt="Notification icon">
+            `;
+            const notificationContent = document.createElement("div");
+            notificationContent.classList.add("notification-content");
+            notificationContent.innerHTML = `
+            <h2 class="notification-title">${notification.title}/h2>
+            <span class="notification-time">${notification.time}</span>
+            <p class="notification-text">${notification.message}</p>`;
+
+            card.appendChild(notificationIcon)
+            card.appendChild(notificationContent);
+
+            if(notification.isNew == true){
+                card.classList.add("new");
+            }
+            updatesList.appendChild(card);
+        })
+    }catch(e){
+        console.log(e);
+    }
+})
